1bcdf34e8aac08c346de7c94fc36ebcee17cd5f3
hashihana <120104605+hashihana@users.noreply.github.com>
2023-01-22 14:55:06 +0000

    [Finish] 19

--- db/migrate/20230116144211_create_post_images.rb
+++ db/migrate/20230116144211_create_post_images.rb
@@ -1,17 +1,11 @@
 class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
-      
-      def change
-  create_table :postimages do |t|
-    t.integer :id
     t.string :shop_name
     t.text :caption
     t.integer :user_id
-  end
-end
-      t.timestamps
+    t.timestamps
     end
   end
-  
 end
+
